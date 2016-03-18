ebb1673a8445413373ee595d9869d508e0ef5f08
Tony Roussel <t.roussel@criteo.com>
2016-03-18 16:44:47 +0100

    init managed test runner file

---
+++ NinjaTurtles/ManagedTestRunners/IManagedTestRunner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaTurtles.ManagedTestRunners
+{
+    interface IManagedTestRunner
+    {
+        /// <summary>
+        /// Runs the tests associate with the current instance asynchronously
+        /// </summary>
+        /// <returns>True if the tests run without failures, false otherwise</returns>
+        bool Start();
+
+        /// <summary>
+        /// Instruct the current instance to wait for the associated run to exit
+        /// </summary>
+        /// <returns></returns>
+        void WaitForExit();
+        
+        /// <summary>
+        /// Instruct the current instance to wait the specified number of milliseconds for the associated run to exit
+        /// </summary>
+        /// <param name="ms"></param>
+        /// <returns>True if the run ended in the specified number of milliseconds, false otherwise</returns>
+        bool WaitForExit(int ms);
+    }
+}
---
+++ NinjaTurtles/NUnitManagedTestRunner.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Core;
+using NUnit.Core.Filters;
+using NUnit.Util;
+
+namespace NinjaTurtles.ManagedTestRunners
+{
+    class NUnitManagedTestRunner : IManagedTestRunner
+    {
+        private readonly TestDirectory _testDirectory;
+        private readonly string _testAssemblyLocation;
+        private readonly string[] _testsToRun;
+        private ProxyTestRunner _remoteTestRunner;
+        private Task _task;
+
+        public int ExitCode { get; private set; }
+
+        public NUnitManagedTestRunner(TestDirectory testDirectory, string testAssemblyLocation, IEnumerable<string> testsToRun)
+        {
+            var testAssemblyMutantLocation = Path.Combine(testDirectory.FullName, Path.GetFileName(testAssemblyLocation));
+
+            _testDirectory = testDirectory;
+            _testAssemblyLocation = testAssemblyMutantLocation;
+            _testsToRun = testsToRun.ToArray();
+            _task = new Task(DoTests);
+            _remoteTestRunner = new RemoteTestRunner();
+            ExitCode = -1;
+        }
+
+        public bool Start()
+        {
+            _task.Start();
+            return true;
+        }
+
+        public void WaitForExit()
+        {
+            _task.Wait();
+        }
+
+        public bool WaitForExit(int ms)
+        {
+            if (ExitCode != -1)
+                return (true);
+            Thread.Sleep(ms);
+            if (ExitCode != -1)
+                return (true);
+            _remoteTestRunner.CancelRun();
+            return (false);
+        }
+
+        private void DoTests()
+        {
+            var currentOut = Console.Out;
+            TestPackage testPackage = new TestPackage(_testAssemblyLocation);
+
+            _remoteTestRunner.Load(testPackage);
+            TestExecutionContext.CurrentContext.TestPackage.Settings.Add("StopOnError", true);
+            string[] tofilter = _testsToRun;
+            TestFilter filter = new SimpleNameFilter(tofilter);
+            //TestResult testResult = _remoteTestRunner.Run(new NullListener(), filter, false, LoggingThreshold.Off);
+            TestResult testResult = _remoteTestRunner.Run(new NullListener(), TestFilter.Empty, false, LoggingThreshold.Off);
+            ExitCode = testResult.IsSuccess ? 0 : 1;
+            Console.SetOut(currentOut);
+        }
+    }
+}
