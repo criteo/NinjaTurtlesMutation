e9e80849cafaf02b77b0c385c64dfed064a21279
Tony Roussel <t.roussel@criteo.com>
2016-04-12 10:53:10 +0200

    Add summary about which file contain surviving mutants

--- NinjaTurtles.Console/Reporting/MutationTestingReportSummary.cs
+++ NinjaTurtles.Console/Reporting/MutationTestingReportSummary.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using NinjaTurtles.Reporting;
 
@@ -9,26 +10,43 @@ namespace NinjaTurtles.Console.Reporting
         private int _mutantsCount;
         private int _mutantsKilledCount;
 
-        private readonly List<string> _methodsWithNoTest;
+        private readonly IList<string> _methodsWithNoTest;
+
+        private readonly IList<SourceFileReportSummary> _survivingMutantsSources;
 
         public MutationTestingReportSummary()
         {
             _mutantsCount = 0;
             _mutantsKilledCount = 0;
             _methodsWithNoTest = new List<string>();
+            _survivingMutantsSources = new List<SourceFileReportSummary>();
         }
 
         public int MutantsCount { get { return _mutantsCount; } }
 
-        public int MutantsKilledCount { get { return _mutantsKilledCount; } } 
+        public int MutantsKilledCount { get { return _mutantsKilledCount; } }
+
+        private void MergeSourceFileReport(MutationTestingReport report)
+        {
+            foreach (var sourceFile in report.SourceFiles)
+            {
+                if (_survivingMutantsSources.Any(s => s.Url == sourceFile.Url) ||
+                    sourceFile.SequencePoints.All(sp => sp.AppliedMutants.All(am => am.Killed)))
+                    continue;
+                _survivingMutantsSources.Add(new SourceFileReportSummary(sourceFile));
+            }
+        }
 
         public void MergeMutationTestReport(MutationTestingReport report)
         {
+            if (!report.TestsFounded)
+            {
+                _methodsWithNoTest.Add(report.MethodFullname);
+                return;
+            }
             _mutantsCount += report.MutantsCount;
             _mutantsKilledCount += report.MutantsKilledCount;
-
-            if (!report.TestsFounded)
-                _methodsWithNoTest.Add(report.MethodFullname);
+            MergeSourceFileReport(report);
         }
 
         public double GetMutationScore()
@@ -46,12 +64,24 @@ namespace NinjaTurtles.Console.Reporting
                 builder.AppendFormat("\t{0}\n", methodName);
         }
 
+        private void SurvivingMutantsSummaryToStringHelper(StringBuilder builder)
+        {
+            if (_survivingMutantsSources.Count == 0)
+                return;
+            builder.Append("Surviving mutants info:\n");
+            foreach (var sourceFile in _survivingMutantsSources)
+            {
+                builder.AppendFormat("{0}\n", sourceFile.Url);
+            }
+        }
+
         public override string ToString()
         {
             StringBuilder builder = new StringBuilder();
             double mutationScore = GetMutationScore();
 
             MethodWithNoTestsToStringHelper(builder);
+            SurvivingMutantsSummaryToStringHelper(builder);
             builder.AppendFormat("Mutation score: {0:0.##} ({1} / {2})", mutationScore, _mutantsKilledCount, _mutantsCount);
             return builder.ToString();
         }
---
+++ NinjaTurtles.Console/Reporting/SourceFileReportSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NinjaTurtles.Reporting;
+
+namespace NinjaTurtles.Console.Reporting
+{
+    internal class SourceFileReportSummary
+    {
+        public string Url { get; private set; }
+
+        public string Filename { get; set; }
+
+        public SourceFileReportSummary(SourceFile sourceFile)
+        {
+            Url = sourceFile.Url;
+            Filename = sourceFile.FileName;
+        }
+    }
+}
