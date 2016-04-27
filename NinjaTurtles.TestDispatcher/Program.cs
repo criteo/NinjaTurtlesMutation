4f5b62524a3e2d6ce3f9c07c9d2bf77fe5b990b4
Tony Roussel <t.roussel@criteo.com>
2016-04-27 13:56:23 +0200

    clean using

--- NinjaTurtles.TestDispatcher/Program.cs
+++ NinjaTurtles.TestDispatcher/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using NinjaTurtles.ServiceTestRunnerLib;
