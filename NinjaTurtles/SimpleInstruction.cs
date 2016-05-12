509fa0f4d42bc6c004a28bd5d7c7d2a93ddbaf23
Tony Roussel <t.roussel@criteo.com>
2016-05-12 15:12:16 +0200

    Add SimpleInstruction struct for opcode and operand representation

---
+++ NinjaTurtles/SimpleInstruction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil.Cil;
+
+namespace NinjaTurtles
+{
+    public struct SimpleInstruction
+    {
+        public OpCode opcode;
+        public object operand;
+
+        public SimpleInstruction(Instruction instruction)
+        {
+            opcode = instruction.OpCode;
+            operand = instruction.Operand;
+        }
+    }
+}
