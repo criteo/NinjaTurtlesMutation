34871d4f1f625ffa3d801ecd09509fc99631c720
Tony Roussel <t.roussel@criteo.com>
2016-05-24 14:43:12 +0200

    Add basic lib for command exchange between NTM and dispatcher

---
+++ NinjaTurtlesMutation/ServiceTestRunnerLib/Utilities/CommandExchanger.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace NinjaTurtlesMutation.ServiceTestRunnerLib.Utilities
+{
+
+    public static class CommandExchanger
+    {
+        private const string TRANSFER_START = "STARTSYNC";
+        private const string TRANSFER_STOP = "ENDSYNC";
+
+        public static class Commands
+        {
+            public const string STOP = "stop";
+        }
+
+        public static void SendData(StreamWriter sw, string cmd)
+        {
+            sw.WriteLine(TRANSFER_START);
+            sw.WriteLine(cmd);
+            sw.WriteLine(TRANSFER_STOP);
+            sw.Flush();
+        }
+
+        public static string ReadACommand(StreamReader sr)
+        {
+            string lineBuf = "";
+            string cmd = "";
+            while (lineBuf != null && lineBuf != TRANSFER_START)
+                lineBuf = sr.ReadLine();
+            if (lineBuf == null)
+                throw new IOException();
+            while (true)
+            {
+                lineBuf = sr.ReadLine();
+                if (lineBuf == null || lineBuf == TRANSFER_STOP)
+                    break;
+                cmd += lineBuf;
+            }
+            if (lineBuf == null)
+                throw new IOException();
+            return cmd;
+        }
+    }
+}
