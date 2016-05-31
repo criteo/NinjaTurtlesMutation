b71c5cce0a07cdd581c19b4a05fdb08921668cf0
Tony Roussel <t.roussel@criteo.com>
2016-05-31 17:32:24 +0200

    clean root

--- FormatOutput.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-copy /y NinjaTurtles.Tests\bin\Debug\SampleReport.xml .
-transform SampleReport.xml ReportXslt.xslt -o:SampleOutput.html
-del NinjaTurtles.Tests\bin\Debug\SampleReport.xml
